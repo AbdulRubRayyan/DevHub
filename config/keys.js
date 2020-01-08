b88cfdbe20ced3ad2cc1515fe07e042a3fa6c480
abdulrubrayyan <abdulrubrayyan@gmail.com>
2020-01-08 19:38:23 +0530

    Validation done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/devhub",
-  secretOrKey: "secret"
+  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  secretOrKey: 'secret'
 };
