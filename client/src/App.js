71c6dcc3fbde3d01a8d31a913a514312586b25b7
abdulrubrayyan <abdulrubrayyan@gmail.com>
2020-01-19 13:14:12 +0530

    Registratio UI and Validation done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
