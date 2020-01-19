acf96ce39fa0bbbbf135d2bf454e3f50e2728e81
abdulrubrayyan <abdulrubrayyan@gmail.com>
2020-01-19 11:44:22 +0530

    Landing page done

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <div className="landing">
+          <div className="dark-overlay landing-inner text-light">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">Connect with Geeks</h1>
+                  <p className="lead">
+                    {" "}
+                    Join our awesome network of learning developers, create a
+                    profile, and post a problem for others to help you out.
+                  </p>
+                  <hr />
+                  <a href="register.html" className="btn btn-lg btn-info mr-2">
+                    Sign Up
+                  </a>
+                  <a href="login.html" className="btn btn-lg btn-light">
+                    Login
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
